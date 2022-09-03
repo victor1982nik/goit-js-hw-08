1247a71277ba2607717b5ff3fcc475119a9c8f3d
Victor Nikolenko <victor1982nik@gmail.com>
2022-09-03 17:55:27 +0300

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import { throttle } from 'lodash';
+import { getData, saveData } from './storage';
 
+const keyLocalStorage = 'feedback-form-state';
+const formRef = document.querySelector(".feedback-form");
+
+const email = formRef.elements.email;
+const message = formRef.elements.message;
+
+onInit();
+
+function onInit() {   
+    const savedData = getData(keyLocalStorage);
+    
+    if (savedData) {
+        email.value = savedData.email;
+        message.value = savedData.message;
+    }
+}
+
+formRef.addEventListener('input', throttle(handleInput, 500));
+formRef.addEventListener('submit', handleSubmit);
+
+function handleInput(e) {    
+    saveData({ email: email.value, message: message.value }, keyLocalStorage);
+}
+
+function handleSubmit(e) {    
+    e.preventDefault();
+    console.log({ email: email.value, message: message.value });
+    email.value = '';
+    message.value = '';
+    localStorage.clear();
+}
