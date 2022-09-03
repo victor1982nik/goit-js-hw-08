9cf41aba13d90b75e888f6141bd9607d94032d79
Victor Nikolenko <victor1982nik@gmail.com>
2022-09-03 17:45:58 +0300

    update 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
+import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+//console.log(SimpleLightbox);
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+render(galleryItems);
+
+function render(gallery) {
+    const arr = gallery.map(({original, preview, description }) => {                 
+        return  `<li><a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a></li>`;        
+    }).join('');
+    
+    galleryRef.insertAdjacentHTML('beforeend', arr); 
+}
+
+let gallery = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250 });
+gallery.on('show.simplelightbox', function (e) {
+	console.log(e);
+});
